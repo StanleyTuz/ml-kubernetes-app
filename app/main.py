00618ccdfb781b887a57607b81dbcd6c05b6cd86
StanleyTuz <stanley.tuznik@gmail.com>
2025-08-22 11:50:14 -0400

    add promotheus metrics endpoint

--- app/main.py
+++ app/main.py
@@ -5,10 +5,19 @@ from pydantic import BaseModel
 import random
 from utils import configure_logging
 from time import time, sleep
+
+from prometheus_client import Counter, Histogram, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+
+
 app = fastapi.FastAPI()
 
 # configure logging
 logger = configure_logging()
+
+# --- Prometheus metrics ---
+REQUEST_COUNT = Counter(name="predictions_total", documentation="Total number of predictions")
+REQUEST_LATENCY = Histogram(name="prediction_latency_seconds", documentation="Latency of prediction requests")
 
 def log_event(event: dict):
     """Write structured log as JSON"""
@@ -25,16 +34,29 @@ async def make_prediction(prediction_request: PredictionRequest):
     pred = random.random()
     # Fake latency
     sleep(random.random())
+    end = time()
+    latency_ms = (end - start) * 1000
 
-    # Structured log
+    # Create and emit structured log
     log = {
         "timestamp": time(),
         "endpoint": "/predict",
-        "input": prediction_request.dict(),
+        "input": prediction_request.model_dump(),
         "prediction": pred,
-        "latency_ms": (time() - start) * 1000,
+        "latency_ms": latency_ms,
     }
     log_event(log)  # emit
     
-    # Return a dummy prediction response
+    # Update metrics
+    REQUEST_COUNT.inc()
+    REQUEST_LATENCY.observe(time() - start) # total latency
+
+    # Return the response
     return {"prediction": pred}
+
+
+# --- Expose a metrics endpoint ---
+@app.get("/metrics")
+def metrics():
+    """This is what Prometheus will scrape."""
+    return fastapi.Response(content=generate_latest(), media_type=CONTENT_TYPE_LATEST)
